1507ac861566b83e11c3ede9d21d2db83eca7e7e
maranimitchelle@gmail.com <mitch@DESKTOP-PMCR53J.localdomain>
2025-12-11 16:47:20 +0300

    initialize and setup react frontend

---
+++ frontend/src/pages/authentication/Register.jsx
@@ -0,0 +1,8 @@
+export default function Register() {
+    return (
+        <div>
+        <h1>Sign up</h1>
+        {/* Admin functionalities go here */}
+        </div>
+    )
+}
---
+++ frontend/src/pages/vendor/VendorDashboard.jsx
@@ -0,0 +1,8 @@
+//main vendor interface
+export default function VendorDashboard() {
+    return (
+        <div>
+        <h1>Vendor Check-In Page</h1>
+        </div>
+    )
+}
